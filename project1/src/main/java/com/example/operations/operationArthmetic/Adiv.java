74c29a98bdde8c240f6f0cfabebf5aa8cefb1ff6
RodrigoAar <ajm22279@uvg.edu.gt>
2025-02-21 12:16:02 -0600

    Implementación de operation en Adiv
    Se implementó la interfaz para la división de dos números o de 1/n cuando se coloca (/n)

---
+++ project1/src/main/java/com/example/operations/operationArthmetic/Adiv.java
@@ -0,0 +1,47 @@
+package com.example.operations.operationArthmetic;
+
+import com.example.operations.Operation;
+import com.example.utils.LoggerManager;
+
+public class Adiv implements Operation{
+    private static final String CATEGORY = "arthmetic";
+
+    @Override
+    public Object execute(Object... args){
+        if (args.length > 2) {
+            LoggerManager.logWarning(CATEGORY, "must have two or one operands");
+            return null;
+        }
+
+        double result = 0;
+        boolean hasDouble = false;
+        boolean hasFloat = false;
+
+        for (Object arg : args) {
+            if (arg == null) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, null);
+                return null;
+            } else if (!(arg instanceof Number)) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, arg.getClass());
+                return null;
+            }
+        
+            Number num = (Number) arg;
+
+            if (num instanceof Double) hasDouble = true;
+            if (num instanceof Float) hasFloat = true;
+        }
+
+        if(args.length == 1){
+            result = 1 / (double) args[0];
+        }else{
+            result = (double) args[0] / (double) args[1];
+        }
+        
+
+        if (hasDouble) return result;
+        if (hasFloat) return (float) result;
+        return (int) result;
+    }
+
+}
