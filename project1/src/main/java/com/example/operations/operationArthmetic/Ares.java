7cea141839d23d771179de35d4a09c3ad186c77f
RodrigoAar <ajm22279@uvg.edu.gt>
2025-02-21 12:20:17 -0600

    Se implementó la interfaz Operation
    Se implementó la interfaz y se modificó el método execute para calcular el residuo entre dos números

--- project1/src/main/java/com/example/operations/operationArthmetic/Ares.java
+++ project1/src/main/java/com/example/operations/operationArthmetic/Ares.java
@@ -1,5 +1,42 @@
 package com.example.operations.operationArthmetic;
 
-public class Ares {
+import com.example.operations.Operation;
+import com.example.utils.LoggerManager;
 
+public class Ares implements Operation{
+    private static final String CATEGORY = "arthmetic";
+
+    @Override
+    public Object execute(Object... args){
+        if (!(args.length == 2)) {
+            LoggerManager.logWarning(CATEGORY, "must have two operands");
+            return null;
+        }
+
+        double result = 0;
+        boolean hasDouble = false;
+        boolean hasFloat = false;
+
+        for (Object arg : args) {
+            if (arg == null) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, null);
+                return null;
+            } else if (!(arg instanceof Number)) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, arg.getClass());
+                return null;
+            }
+        
+            Number num = (Number) arg;
+
+            if (num instanceof Double) hasDouble = true;
+            if (num instanceof Float) hasFloat = true;
+        }
+
+        result = (double) args[0] % (double) args[1];
+        
+
+        if (hasDouble) return result;
+        if (hasFloat) return (float) result;
+        return (int) result;
+    }
 }
