b083b8b481baf86ce5402fe68a5df095029fea71
RodrigoAar <ajmacrodrigo@gmail.com>
2025-02-21 11:46:56 -0600

    Implementación de la interfaz Operation
    Se implementó la interfaz Operation y se reescribió el método execute para esta operación.

--- project1/src/main/java/com/example/operations/operationArthmetic/Asub.java
+++ project1/src/main/java/com/example/operations/operationArthmetic/Asub.java
@@ -1,3 +1,42 @@
-public class Asub{
-    
+package com.example.operations.operationArthmetic;
+
+import com.example.operations.Operation;
+import com.example.utils.LoggerManager;
+
+public class Asub implements Operation{
+    private static final String CATEGORY = "arthmetic";
+
+    @Override
+    public Object execute(Object... args){
+        if (args.length < 2) {
+            LoggerManager.logWarning(CATEGORY, "must have at least two operands");
+            return null;
+        }
+
+        double result = 0;
+        boolean hasDouble = false;
+        boolean hasFloat = false;
+
+        for (Object arg : args) {
+            if (arg == null) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, null);
+                return null;
+            } else if (!(arg instanceof Number)) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, arg.getClass());
+                return null;
+            }
+        
+            Number num = (Number) arg;
+
+            if (num instanceof Double) hasDouble = true;
+            if (num instanceof Float) hasFloat = true;
+        }
+
+        result = (double) args[0] - (double) args[1];
+        
+
+        if (hasDouble) return result;
+        if (hasFloat) return (float) result;
+        return (int) result;
+    }
 }
