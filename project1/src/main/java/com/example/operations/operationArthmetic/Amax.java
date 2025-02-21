80733aadbebc57b1059b245fd440442cf2e211b6
RodrigoAar <ajm22279@uvg.edu.gt>
2025-02-21 13:12:24 -0600

    Clase Amax
    Se creó la clase Amax y se implementó la interfaz operation para modificar el método execute para que dado un conjunto de número se encuentre el mayor de ellos.

---
+++ project1/src/main/java/com/example/operations/operationArthmetic/Amax.java
@@ -0,0 +1,44 @@
+package com.example.operations.operationArthmetic;
+
+import com.example.operations.Operation;
+import com.example.utils.LoggerManager;
+
+public class Amax implements Operation{
+    private static final String CATEGORY = "arthmetic";
+
+     @Override
+    public Object execute(Object... args) {
+        if (args.length < 1) {
+            LoggerManager.logWarning(CATEGORY, "must have at least one operand");
+            return null;
+        }
+
+        double result = (double) args[0];
+        boolean hasDouble = false;
+        boolean hasFloat = false;
+
+        for (Object arg : args) {
+            if (arg == null) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, null);
+                return null;
+            } else if (!(arg instanceof Number)) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, arg.getClass());
+                return null;
+            }
+            
+            Number num = (Number) arg;
+
+            if (num instanceof Double) hasDouble = true;
+            if (num instanceof Float) hasFloat = true;
+            if(result < (double)arg){
+                result = (double) arg;
+            }
+
+        }
+
+
+        if (hasDouble) return result;
+        if (hasFloat) return (float) result;
+        return (int) result;
+    }
+}
