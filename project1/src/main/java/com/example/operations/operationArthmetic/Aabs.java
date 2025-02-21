5e8e0034c85c4f25d716b643632e06048867e676
RodrigoAar <ajm22279@uvg.edu.gt>
2025-02-21 12:57:05 -0600

    Implementación de operation en Aabs
    Se implementó la interfaz Operation y el método execute para calcular el valor absoluto.

--- project1/src/main/java/com/example/operations/operationArthmetic/Aabs.java
+++ project1/src/main/java/com/example/operations/operationArthmetic/Aabs.java
@@ -1,5 +1,42 @@
 package com.example.operations.operationArthmetic;
 
-public class Aabs {
+import com.example.operations.Operation;
+import com.example.utils.LoggerManager;
+
+public class Aabs implements Operation{
+    private static final String CATEGORY = "arthmetic";
+
+     @Override
+    public Object execute(Object... args) {
+        if (args.length != 1) {
+            LoggerManager.logWarning(CATEGORY, "must have one operand");
+            return null;
+        }
+
+        double result = 0;
+        boolean hasDouble = false;
+        boolean hasFloat = false;
+
+        for (Object arg : args) {
+            if (arg == null) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, null);
+                return null;
+            } else if (!(arg instanceof Number)) {
+                LoggerManager.logUnsupportedOperation(CATEGORY, arg.getClass());
+                return null;
+            }
+            
+            Number num = (Number) arg;
+
+            if (num instanceof Double) hasDouble = true;
+            if (num instanceof Float) hasFloat = true;
+        }
+
+        result = Math.abs((double)args[0]);
+
+        if (hasDouble) return result;
+        if (hasFloat) return (float) result;
+        return (int) result;
+    }
 
 }
