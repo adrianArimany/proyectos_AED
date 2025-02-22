c16fea99eb3d4c6d87460206e96248f3a4269da1
Adrian Arimany <176186615+adrianArimany@users.noreply.github.com>
2025-02-21 19:10:30 -0600

    started working on the datamanager.

---
+++ project1/src/main/java/com/example/dataManager/Scanner.java
@@ -0,0 +1,30 @@
+package com.example.dataManager;
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+public class Scanner {
+   /**
+ * 
+ * Instance <- finite state machines {one for each type of token}
+ * 
+ * 
+ * 
+ * Call one line from the file
+ *  
+ * For each character k advance the state machine (starting from state q_0)
+ * 
+ * Then you read a seperator " " identify which finite state machine is in the final state machine "q_f"
+ * 
+ * Add the token to the tokens list
+ * 
+ * 
+ * 
+ * Once the token list is read though, returns the tokensList
+ * 
+ * 
+ * 
+ */ 
+}
--- project1/src/main/java/com/example/objectCompiler/compilerLisp.java
+++ project1/src/main/java/com/example/objectCompiler/compilerLisp.java
@@ -1,9 +1,23 @@
 package com.example.objectCompiler;
-
+/**
+ * Syntax Tree 
+ * 
+ * 
+ */
 public class compilerLisp implements  Icompiler{
     @Override
     public Object compile(String code) {
+    /**
+     * Send the first line to the Scanner to get the token list 
+     * 
+     * Then create the syntax tree with the token list
+     * 
+     * Then Run syntax tree (if there is not error)
+     * 
+     * Then repeat for the rest of the lines (from the file)
+     * 
+     */
         return null;
     }
-    
+
 }
