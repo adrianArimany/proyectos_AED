372d8655d19ed71a47beede3e0f8c04b37a2d745
Adrian Arimany <176186615+adrianArimany@users.noreply.github.com>
2025-02-22 16:44:23 -0600

    .

--- project1/src/main/java/com/example/App.java
+++ project1/src/main/java/com/example/App.java
@@ -6,7 +6,6 @@ package com.example;
  * 
  * IMPORTANT:
  * @TODO
- * - THE LoggerManager is not working properly, is just reeturning null, FIX IT. (I think the issue is with how is being isntace between classes.)
  * - Finish all the other finite statemachines for the other operands and characters and comments etc
  */
 public class App 
