661cb54326cc80a872c6eee7af365bc6bfd4a3f5
Adrian Arimany <176186615+adrianArimany@users.noreply.github.com>
2025-02-22 17:32:56 -0600

    .

--- project1/src/main/java/com/example/utils/TokenType.java
+++ project1/src/main/java/com/example/utils/TokenType.java
@@ -11,11 +11,7 @@ public enum TokenType {
  
  OPERANDARITHMETIC, // + - * / %
  
- CONDITIONALS, // < > <= >= == != 
- 
- Comparison, // < > <= >= == != 
-
- 
+ CONDITIONALS, // < > <= >= == 
 
  NUMBER, // 0, 1,2,3,4,5,6,7,8,9
 
