aad791f1e0623634c525fb9caa75b173da638885
RodrigoAar <ajm22279@uvg.edu.gt>
2025-05-27 21:04:11 -0600

    Update Sample.java
    Clase para "crear" samples a partir de los datos del csv

--- project2/untitled/src/main/java/org/example/Sample.java
+++ project2/untitled/src/main/java/org/example/Sample.java
@@ -3,21 +3,19 @@ package org.example;
 public class Sample {
     private String id;
     private String instrument;
-    private double tempo;
+    private double pitch;
     private double velocity;
-    private double duration;
 
-    public Sample(String id, String instrument, double tempo, double velocity, double duration) {
+    public Sample(String id, String instrument, double pitch, double velocity) {
         this.id = id;
         this.instrument = instrument;
-        this.tempo = tempo;
+        this.pitch = pitch;
         this.velocity = velocity;
-        this.duration = duration;
     }
 
     public String getId() { return id; }
     public String getInstrument() { return instrument; }
-    public double getTempo() { return tempo; }
+    public double getPitch() { return pitch; }
     public double getVelocity() { return velocity; }
-    public double getDuration() { return duration; }
 }
+
